0655fefa137a4fb1d93194fa2f6aa82e17afa6f9
Kviraj <kawa.135viraj@gmail.com>
2024-02-26 17:37:54 +0530

    Next auth bug.

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+import { custom } from 'openid-client';
+
+custom.setHttpOptionsDefaults({
+  timeout: 10000,
+});
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  callbacks:{
+    async session({ session }: any) {
+        console.log(session);
+        const sessionUser = await User.findOne({
+          email: session.user.email,
+        });
+        session.user.id = sessionUser._id.toString();
+        return session;
+      },
+      async signIn({ profile }: any) {
+        try {
+          await connectToDB();
+          const userExist = await User.findOne({
+            email: profile.email,
+          });
+    
+          if (!userExist) {
+            const user = await User.create({
+              email: profile.email,
+              username: profile.name.replace(" ", "").toLowerCase(),
+              image: profile.picture,
+            });
+          }
+        } catch (e) {
+          console.log("Error while signIn", e);
+          return false;
+        }
+      },
+  },
+  
+});
+
+export { handler as GET, handler as POST };
---
+++ utils/database.ts
@@ -0,0 +1,23 @@
+import mongoose, { ConnectOptions }from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("Mongodb is already connected");
+    return;
+  }
+  try {
+    
+    await mongoose.connect(process.env.MONGODB_URI || "", {
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+    });
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (e) {
+    console.log("Error while connecting to the database", e);
+  }
+};
