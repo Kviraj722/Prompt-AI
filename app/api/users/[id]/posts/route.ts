3d7ad395b0d154dc1edfc839c2138fb3b45d04b1
Kviraj <kawa.135viraj@gmail.com>
2024-02-29 11:02:26 +0530

    feat: user profile.

---
+++ app/api/users/[id]/posts/route.ts
@@ -0,0 +1,16 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request: Object, { params }: any) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({
+      creator: params.id,
+    });
+
+    return new Response(JSON.stringify(prompts), { status: 201 });
+  } catch (e) {
+    console.log("Error while fetching the prompts", e);
+    return new Response("Error while fetching the prompts", { status: 500 });
+  }
+};
--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -1,27 +1,43 @@
 "use client";
-import React from "react";
+
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
+
 const PromptCard = ({
   post,
-  handleTagClick,
   handleEdit,
   handleDelete,
+  handleTagClick,
 }: any) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
-  const handleCopyFnc = () => {
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
+  const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied(""), 3000);
+    setTimeout(() => setCopied(false), 3000);
   };
-  console.log("Hello => ", copied);
 
   return (
     <div className="prompt_card">
-      <div className="flex justify-between items-center gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+      <div className="flex justify-between items-start gap-5">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -40,31 +56,44 @@ const PromptCard = ({
           </div>
         </div>
 
-        <div
-          className="copy_btn"
-          onClick={() => {
-            handleCopyFnc();
-          }}
-        >
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
-              copied == post.prompt
+              copied === post.prompt
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
-            alt="copy_image"
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
         </div>
       </div>
+
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
-        className="font-inter text-sm blue_gradient cursor-pointer text-gray-700"
+        className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
